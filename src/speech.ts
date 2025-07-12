3920c343102684bc7fe1a9b464f866191d2d4c80
Atul Varma <varmaa@gmail.com>
2025-07-12 11:28:39 -0400

    Add logic to choose the best Korean voice.

--- src/speech.ts
+++ src/speech.ts
@@ -1,13 +1,54 @@
 /**
- * Returns the first available Korean voice, or null if none found.
+ * Ideally we'd just lean on the browser to choose whatever
+ * Korean voice the user has indicated a preference for at
+ * the browser or OS level, but it doesn't seem like this
+ * is supported well across differennt browsers, e.g.:
+ *
+ *   * Firefox will default to the system's default for
+ *     an utterance's language if it doesn't have a voice
+ *     explicitly set.  `getVoices()` does not have `default`
+ *     set for whatever the system default for a given
+ *     language is, though.
+ *
+ *   * Safari will not vocalize an utterance at all if
+ *     it doesn't have a voice explicitly set. `getVoices()`
+ *     seems to claim that *all* voices are default.
+ *
+ * So we're just going to fall back on choosing some good voices
+ * based on regular expressions.
  */
-function findKoreanVoice(): SpeechSynthesisVoice | null {
+const BEST_VOICES = [
+  // "Yuna" is a voice that sounds pretty good on Apple devices,
+  // and comes in premium and enhanced variants.
+  /^yuna.*premium/i,
+  /^yuna.*enhanced/i,
+  // Fall back to *any* Korean premium or enhanced voice.
+  /premium/i,
+  /enhanced/i,
+  // Fall back to Yuna if nothing else is available, it's still
+  // much better than most of the other pre-installed Korean voices.
+  /^yuna/i,
+];
+
+/**
+ * Returns the best available Korean voice, or null if none found.
+ */
+function findBestKoreanVoice(): SpeechSynthesisVoice | null {
   if (!("speechSynthesis" in window)) {
     return null;
   }
 
-  const voices = speechSynthesis.getVoices();
-  return voices.find((voice) => voice.lang.startsWith("ko")) || null;
+  const koreanVoices = speechSynthesis
+    .getVoices()
+    .filter((voice) => voice.lang.startsWith("ko"));
+  for (const regex of BEST_VOICES) {
+    for (const voice of koreanVoices) {
+      if (regex.test(voice.name)) {
+        return voice;
+      }
+    }
+  }
+  return koreanVoices[0] ?? null;
 }
 
 /**
@@ -15,15 +56,15 @@ function findKoreanVoice(): SpeechSynthesisVoice | null {
  * has a Korean voice available.
  */
 export function supportsKoreanSpeech(): boolean {
-  return findKoreanVoice() !== null;
+  return findBestKoreanVoice() !== null;
 }
 
 /**
- * Use the Web Speech API to vocalize the given Hangul using the first
+ * Use the Web Speech API to vocalize the given Hangul using the best
  * Korean-language voice available.
  */
 export function vocalizeKoreanSpeech(hangul: string) {
-  const koreanVoice = findKoreanVoice();
+  const koreanVoice = findBestKoreanVoice();
   if (!koreanVoice) {
     console.warn("Korean speech synthesis not supported");
     return;
